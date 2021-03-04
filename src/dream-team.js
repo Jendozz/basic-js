819d78494c6508084b08c99e815f376fd5257064
Naumov Evgeniy <n_zhenek@mail.ru>
2021-03-04 19:42:01 +0400

    feat: solved 2 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members == null){
+    return false
+  }
+  str = "";
+  for (let i = 0; i < members.length; i++){
+    if (typeof(members[i]) == typeof(""))
+    {
+      let arr = members[i].split("")
+      for (let k = 0; k < arr.length; k++)
+      {
+        if (arr[k] != " ")
+        {
+          str += arr[k]
+          break;
+        }
+      }
+    }
+  }
+  str = str.toUpperCase();
+  if (str.length == 0){
+    return false;
+  }
+  else{
+    let result = str.split('').sort().join('');
+    return result;
+  }
 };
