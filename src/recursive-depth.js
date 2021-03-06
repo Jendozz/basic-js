0be9a21e0e3c0f4759407b4a818ebe8f9421be62
Naumov Evgeniy <n_zhenek@mail.ru>
2021-03-06 12:41:44 +0400

    feat: solved 4 tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+    calculateDepth(arr) {
+      let result = 1;
+      let count = 0;
+      for (let i = 0; i < arr.length; i++){
+        if (Array.isArray(arr[i]))
+        {
+          count = 1 + this.calculateDepth(arr[i]);
+          if(count > result){
+            result = count;
+          }
+        }
+      }
+      return result;
+  };
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,107 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw new TypeError();
+   }
+
+  let arrCopy = arr.slice()
+  let result = [];
+  let flag = false;
+  for (let i = 0; i < arrCopy.length; i++){
+     if (arrCopy[i] == "--discard-next"){
+      arrCopy = discardNext(arrCopy, i);
+      result = arrCopy;
+      flag = true;
+    }
+    else if(arrCopy[i] == "--discard-prev"){
+      arrCopy = discardPrev(arrCopy, i);
+      result = arrCopy;
+      flag = true;
+    }
+    else if(arrCopy[i] == "--double-next"){
+      arrCopy = doubleNext(arrCopy, i);
+      result = arrCopy;
+      flag = true;
+    }
+    else if(arrCopy[i] == "--double-prev"){
+      arrCopy = doublePrev(arrCopy, i);
+      result = arrCopy;
+      flag = true
+    }
+
+  }
+  if (flag == false)
+  {
+    return arr;
+  }
+  else{
+    result = cleanArr(result);
+    return result
+  }
+ 
+  
+}
+
+
+function discardNext(arrfunc, i){
+  if(i != arrfunc.length - 1 && arrfunc[i+1] != null){
+    arrfunc.splice(i, 1)
+    arrfunc[i] = null;
+  }
+  else{
+    arrfunc.splice(i, 1)
+  }
+  
+return arrfunc;
+}
+
+
+function discardPrev(arrfunc, i){
+  if (i != 0 && arrfunc[i-1] != null){
+    arrfunc.splice(i,1)
+    arrfunc[i-1] = null
+  }
+  else{
+    arrfunc.splice(i,1)
+  }
+  
+return arrfunc;
+}
+
+function doubleNext(arrfunc, i){
+  if(i != arrfunc.length - 1 && arrfunc[i+1] != null){
+    arrfunc.splice(i,1)
+    arrfunc.splice(i, 0 , arrfunc[i])
+  }
+  else{
+    arrfunc.splice(i,1)
+  }
+  return arrfunc;
+}
+
+function doublePrev(arrfunc, i){
+  if (i != arrfunc[0] && arrfunc[i-1] != null){
+    arrfunc.splice(i,1)
+    arrfunc.splice(i, 0, arrfunc[i-1])
+  }
+  else{
+    arrfunc.splice(i, 1)
+  }
+
+return arrfunc;
+}
+
+function cleanArr(resultArr){
+  let clArr = []
+  for(let k = 0; k < resultArr.length; k++){
+
+    if (resultArr[k] != null){
+      clArr.push(resultArr[k])
+    }
+    else{
+      continue;
+    }
+  }
+  return clArr;
+}
