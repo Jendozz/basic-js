2ef0fb5d898b1bab7957b97a0553aa0923e7a101
Naumov Evgeniy <n_zhenek@mail.ru>
2021-03-06 16:13:46 +0400

    feat: solved 6 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,18 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const LN2 = 0.693;
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) != typeof('') || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY){
+    return false;
+  }
+  
+  if (isNaN(Number(sampleActivity)) || Number(sampleActivity) == 0){
+    return false;
+  }
+  
+  const RATE = LN2 / HALF_LIFE_PERIOD;
+  age = (Math.log(MODERN_ACTIVITY/sampleActivity))/RATE;
+  age = Math.ceil(age);
+  return age;
 };
